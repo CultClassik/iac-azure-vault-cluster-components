2ab86fb1dff44eed549c532431858e6d31e63c28
Chris Diehl <chris.diehl@verituity.com>
2022-11-28 17:04:34 -0600

    update backend

--- backends/nonprod.tf
+++ backends/nonprod.tf
@@ -4,5 +4,6 @@ terraform {
     key                  = "vault-components.nonprod.tfstate"
     resource_group_name  = "tfstate-rg-vty-nop-eastus2"
     storage_account_name = "tfstatevtynop"
+    subscription_id      = "3810f594-f91b-404a-b6eb-ebf9b9e4f62c"
   }
 }

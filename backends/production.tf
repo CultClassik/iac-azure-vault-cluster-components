0bfb652fb277483597bd7c9c9e65b798c8d4069a
Chris Diehl <chris.diehl@verituity.com>
2022-11-28 17:49:22 -0600

    update vars for production

--- backends/production.tf
+++ backends/production.tf
@@ -4,5 +4,6 @@ terraform {
     key                  = "vault-components.production.tfstate"
     resource_group_name  = "tfstate-rg-vty-prd-eastus2"
     storage_account_name = "tfstatevtyprd"
+    subscription_id      = "951a295c-41c3-45ec-8b32-2149f85581c1"
   }
 }
